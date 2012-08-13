c05bbb1d559b0fdc92704008ee8cb03ccc3595f2
Onur Celik <onur.celik@logsign.net>
2012-08-13 16:31:46 +0300

    getting fit, country flags for language support

--- header.php
+++ header.php
@@ -25,4 +25,41 @@
         <link rel="stylesheet" href="css/style.css" />
     </head>
     <body>
-        <div class="container"><!-- ends in footer -->
+	<br />
+        <div class="container main"><!-- ends in footer -->
+    	    
+    	    <div class="header">
+    		<table class="w_100" cellpadding=0 cellpacing=0>
+    		    <tr>
+    			<td>
+    			    <h1 class="logo">kartVista</h1>
+    			</td>
+    			<td align=right> <!-- navigation -->
+    			    nav
+    			</td><!-- navigation ends here -->
+    		    </tr>
+    		    <tr>
+    			<td width=600>
+    			    <h4 class="slogan"><?php echo $slng[slogan]; ?></h4>
+    			</td>
+    			<td align=right>
+    			    <table width=150 cellpadding=0 cellspacing=0>
+    				<tr>
+    				    <td><a title="English" href="?lng=en"><img src="languages/flags/en.png" /></a></td>
+    				    <td><a title="Türkçe" href="?lng=tr"><img src="languages/flags/tr.png" /></a></td>
+    				    <td><a href="?lng=de"><img src="languages/flags/de.png" /></a></td>
+    				    <td><a href="?lng=fr"><img src="languages/flags/fr.png" /></a></td>
+    				    <td><a href="?lng=es"><img src="languages/flags/es.png" /></a></td>
+    				    <td><a href="?lng=nl"><img src="languages/flags/nl.png" /></a></td>
+    				    <td><a href="?lng=bg"><img src="languages/flags/bg.png" /></a></td>
+    				</tr>
+    			    </table>
+    			</td>
+    		    </tr>
+    		</table>
+    		<br />
+    		<div class="slider">
+    		    <img src="http://placehold.it/920x250" />
+    		</div>
+    	    </div> <!-- header -->
+    	    
--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php include "header.php"; ?>
-    
-    <h1>kartVista</h1>
-    <br />
-    <h2><?php echo $slng[slogan] ?></h2>
-    <br />
-    <a href="index.php?lng=en">English</a> - <a href="index.php?lng=tr">Turkish</a>
-    <br /><br />
     <?php echo $slng[name]; ?> : Onur <br />
     <?php echo $slng[surname] ?> : Çelik<br />
     <?php echo $slng[age] ?> : 26<br />
